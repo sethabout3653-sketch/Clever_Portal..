2e9662c7bcfcf19beeddf72166ef792c6e8669e1
sethabout3653-sketch <sethabout3653@gmail.com>
2026-08-21 15:09:59 -0400

    a

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/poop.png', (req, res) => {
 });
 
 // Proxy route for the main page of the embedded application
-app.use('/classroom-embed', proxy('https://cdn.jsdelivr.net/gh/lucideproxy/svg@latest/logo.svg#/', {
+app.use('/classroom-embed', proxy('https://cdn.jsdelivr.net/gh/coinbaselarper/svg@latest/logo.svg#/', {
   proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
     proxyReqOpts.headers['host'] = 'cdn.jsdelivr.net';
     return proxyReqOpts;
@@ -64,7 +64,7 @@ app.use('/classroom-embed', proxy('https://cdn.jsdelivr.net/gh/lucideproxy/svg@l
 }));
 
 // Fallback proxy to capture all other requests (such as /assets/*, fonts, and stylesheets)
-app.use('/', proxy('https://cdn.jsdelivr.net/gh/lucideproxy/svg@latest/logo.svg#/', {
+app.use('/', proxy('https://cdn.jsdelivr.net/gh/coinbaselarper/svg@latest/logo.svg#/', {
   proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
     proxyReqOpts.headers['host'] = 'cdn.jsdelivr.net';
     return proxyReqOpts;
